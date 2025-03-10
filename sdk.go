415fe28d9aa72f07d65c4490e89b1c6c7939800d
yuuki798 <b19357160114@gmail.com>
2025-03-10 20:21:31 +0800

    chore: grpc注册

--- sdk.go
+++ sdk.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type GatewaySDK struct {
@@ -23,6 +24,14 @@ type RouteInfo struct {
 	ServiceName string `json:"service_name"`
 	Path        string `json:"path"`
 	Method      string `json:"method"`
+}
+
+type GrpcApiInfo struct {
+	ServiceName string `json:"service_name"`
+	Path        string `json:"path"`
+	Method      string `json:"method"`
+	GrpcService string `json:"grpc_service"`
+	GrpcMethod  string `json:"grpc_method"`
 }
 
 func (sdk *GatewaySDK) RegisterRoute(route RouteInfo) error {
@@ -70,4 +79,52 @@ func (sdk *GatewaySDK) AutoRegisterGinRoutes(router *gin.Engine, serviceName str
 
 	// 批量注册路由
 	return sdk.RegisterRoutes(routes)
+}
+
+func grpcMethodName2Snake(methodName string) string {
+	// 处理method.Name 从驼峰变成蛇形
+	// 例如：SayHello转换为 say_hello
+	var res string
+	for i, r := range methodName {
+		if i > 0 && r >= 'A' && r <= 'Z' {
+			res += "_" + strings.ToLower(string(r))
+		} else {
+			res += strings.ToLower(string(r))
+		}
+	}
+	return res
+}
+
+func grpcMethodName2HttpPath(methodName string) string {
+	// 处理method.Name 从驼峰变成http路由
+	// 例如：SayHello转换为 say/hello
+	var res string
+	for i, r := range methodName {
+		if i > 0 && r >= 'A' && r <= 'Z' {
+			res += "/" + strings.ToLower(string(r))
+		} else {
+			res += strings.ToLower(string(r))
+		}
+	}
+	return res
+}
+
+func (sdk *GatewaySDK) RegisterGRPCRoutes(routes []GrpcApiInfo) error {
+	for _, route := range routes {
+		jsonData, err := json.Marshal(route)
+		if err != nil {
+			return err
+		}
+
+		resp, err := http.Post(sdk.GatewayURL+"/gateway/api", "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("failed to register GRPC route: %s", resp.Status)
+		}
+	}
+	return nil
 }
